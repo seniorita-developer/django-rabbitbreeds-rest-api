43a271f890697b3c4c4b49fb089aca2a1e5cfe8e
seniorita-developer <valeriia.zabkowska@gmail.com>
2020-05-07 16:20:55 +0200

    Throttle rates max 20/min added

--- rabbitbreedapi/rabbitbreedapi/settings.py
+++ rabbitbreedapi/rabbitbreedapi/settings.py
@@ -112,5 +112,11 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_THROTTLE_CLASSES': (
+            'rest_framework.throttling.AnonRateThrottle',
+    ),
+    'DEFAULT_THROTTLE_RATES': {
+      'anon': '20/min',
+  }
 }
