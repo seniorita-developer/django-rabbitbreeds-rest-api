1b4cb13eb78de1552de7d59c7640f40dee0cccf4
seniorita-developer <valeriia.zabkowska@gmail.com>
2020-05-08 12:50:27 +0200

    Readonly permission fixed

--- rabbitbreedapi/breedapi/views.py
+++ rabbitbreedapi/breedapi/views.py
@@ -14,16 +14,15 @@ class BreedViewSet(viewsets.ModelViewSet):
     http_method_names = ['get']
 
 
-@permission_classes((permissions.AllowAny,))
+@permission_classes((permissions.IsAuthenticatedOrReadOnly,))
 class BreedsWithImagesViewSet(viewsets.ViewSet):
-    """A ViewSet for breeds with links to images provided"""
     def list(self, request):
         queryset = Breed.objects.filter(image__contains='.jpg')
         serializer = BreedSerializer(queryset, many=True)
         return Response(serializer.data)
 
 
-@permission_classes((permissions.AllowAny,))
+@permission_classes((permissions.IsAuthenticatedOrReadOnly,))
 class GetRandomBreed(generics.ListAPIView):
     queryset = Breed.objects.all().order_by('?')[:1]
     serializer_class = BreedSerializer
