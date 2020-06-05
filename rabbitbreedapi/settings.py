0933a3bd263610c33902709ecc6194bded993b43
seniorita-developer <valeriia.zabkowska@gmail.com>
2020-06-05 17:08:21 +0200

    Fixes

--- rabbitbreedapi/settings.py
+++ rabbitbreedapi/settings.py
@@ -1,9 +1,9 @@
 import os
-# from secret_settings import SECRET_KEY
+from secret_settings import SECRET_KEY
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = '26cufhw%^d!=#q^=mb86!6*2-gf^kvqge0amvv-j!-hh%!&drg'
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
@@ -11,7 +11,7 @@ SECRET_KEY = '26cufhw%^d!=#q^=mb86!6*2-gf^kvqge0amvv-j!-hh%!&drg'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ancient-thicket-71997.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'ancient-thicket-71997.herokuapp.com']
 
 
 # Application definition
