11f45c5370f8db894c2632202eebf5a7028bd297
seniorita-developer <valeriia.zabkowska@gmail.com>
2020-06-05 16:40:25 +0200

    Fixes

--- rabbitbreedapi/settings.py
+++ rabbitbreedapi/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = '26cufhw%^d!=#q^=mb86!6*2-gf^kvqge0amvv-j!-hh%!&drg'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ancient-thicket-71997.herokuapp.com']
 
 
 # Application definition
