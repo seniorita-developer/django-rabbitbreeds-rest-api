e45f7354848f3842582ce7c2f982237965f1373d
seniorita-developer <valeriia.zabkowska@gmail.com>
2021-03-02 00:00:28 +0100

    Add heroku to allowed host

--- rabbitbreedapi/settings.py
+++ rabbitbreedapi/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'ancient-thicket-71997.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'rabbit-breed-api.herokuapp.com']
 
 # Application definition
 
